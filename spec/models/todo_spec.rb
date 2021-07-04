6a50da851d4a39d5b7b4e0163f0921cf364f8a8e
Aiden Mendez <aidenmendez@gmail.com>
2021-07-04 13:25:17 -0600

    Add due_date to model test

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Todo do
   it { is_expected.to be_mongoid_document }
   it { is_expected.to have_timestamps }
-  it { is_expected.to have_fields(:title, :order, :url, :completed) }
+  it { is_expected.to have_fields(:title, :order, :url, :completed, :due_date) }
 end
